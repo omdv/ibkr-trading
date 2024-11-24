392298776a718fc99b302057ed278700782092f8
Oleg Medvedev <omdv@protonmail.com>
2024-11-24 12:31:18 -0600

    bump pulumi nix package

--- pulumi.nix
+++ pulumi.nix
@@ -2,10 +2,10 @@
 
 pkgs.stdenv.mkDerivation {
   pname = "pulumi";
-  version = "3.128.0";
+  version = "3.141.0";
   src = pkgs.fetchurl {
-    url = "https://get.pulumi.com/releases/sdk/pulumi-v3.128.0-linux-x64.tar.gz";
-    sha256 = "sha256-0CtD2FKGWsG2ui0ggyxCgk1qcfBbZlg5RYqcbBrDypU=";
+    url = "https://get.pulumi.com/releases/sdk/pulumi-v3.141.0-linux-x64.tar.gz";
+    sha256 = "sha256-gbUeSuxw58jyKHou2It4AfTK3gdMrMnzzDyoUsrkdwA=";
   };
   installPhase = ''
     mkdir -p $out/bin
